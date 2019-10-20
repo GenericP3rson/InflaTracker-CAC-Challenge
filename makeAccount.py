Got sign up working and fixed a small bug in makeAccount.py.

makeAccount.py
@@ -15,6 +15,7 @@ class CSV():
         self.words = list(self.words)
         # self.statsList = [eval(i) for i in self.stats]
         self.stats = list(self.stats)
+        self.profileImg = list(self.profileImg)
         print(self.stats)
         self.authenticated = False
         self.user = ""
@@ -31,6 +32,11 @@ class CSV():
             if hashing.hashTag(pas) == self.userToPass[user]:
                 self.authenticated = True
                 self.user = user
+                # We forgot to edit self.ind....
+                for i in range(len(self.users)):
+                    if self.users[i] == user:
+                        self.ind = i
+                self.theProfileImg = self.profileImg[self.ind]
                 self.ind = self.userToNum[self.user]
                 self.userUsername = self.users[self.ind]
                 self.userRealName = self.names[self.ind]
@@ -41,15 +47,17 @@ class CSV():
                 return False
         except:
             print("INVALID CREDENTIALS")
+            return False
     def rereadCSV(self, accounts = "Accounts"):
         '''
         It updates the arrays with the data.
         '''
-        self.users, self.names, self.words, self.stats = (pd.read_csv(f"{accounts}.csv").values.T)
+        self.users, self.names, self.words, self.stats, self.profileImg = (pd.read_csv(f"{accounts}.csv").values.T)
         self.users = list(self.users)
         self.names = list(self.names)
         self.words = list(self.words)
         self.stats = list(self.stats)
+        self.profileImg = list(self.profileImg)
         # self.statsList = [eval(i) for i in self.stats]
         self.userToNum = {user: num for num, user in enumerate(self.users)}
         self.userToPass = {user:pas for user, pas in zip(self.users, self.words)}
@@ -62,13 +70,17 @@ class CSV():
         '''
         if user in self.users:
             print("ERROR: PICK ANOTHER NAME")
-            return
+            return False
+        self.user = user
         self.users.append(user)
         self.names.append(name)
         self.words.append(hashing.hashTag(pas))
         self.stats.append(stat)
+        self.profileImg.append(F"https://picsum.photos/id/{len(self.user)}/200/300")
+        self.theProfileImg = F"https://picsum.photos/id/{len(self.user)}/200/300"
         self.updateCSV()
         self.rereadCSV()
+        return True
     def editStats(self, newStat):
         if self.authenticated:
             self.stats[self.ind] = newStat
@@ -103,4 +115,12 @@ class CSV():
         else: 
             return 0
     def updateCSV(self):
-        pd.DataFrame(np.array([self.users, self.names, self.words, self.stats]).T).to_csv(f"Accounts.csv", header=["USERNAMES", "NAMES", "PASSWORDS", "INFO"], index=False)
+        print(np.array([self.users, self.names, self.words, self.stats, self.profileImg]).T)
+        print(len(self.users), len(self.names), len(self.words), len(self.stats), len(self.profileImg))
+        pd.DataFrame({
+            "USERNAMES": self.users,
+            "NAMES": self.names,
+            "PASSWORDS": self.words,
+            "INFO": self.stats,
+            "PROFILE_IMG": self.profileImg
+        }).to_csv("Accounts.csv", index=False)

understanding.py
@@ -21,44 +21,41 @@ except:
    # Now write it into a new file.
    pd.DataFrame(np.array(data).T).to_csv(f"{processedData}.csv", header=False, index=False)
 
-
+user = makeAccount.CSV()
 class Crohns():
     def __init__(self, accounts = "Accounts"):
         self.food = data[0]
         self.ing = data[1]
         self.ingEaten = [] # Key: [[ingName, total # of inflams, total # of times eaten, % of inflams]]
-        self.accountInfo = {}
+        self.accountInfo = user
         # Okay, automatically when we start the server the user isn't logged in.
         @app.route('/')
         def _home():
-            return render_template('profile.html', logo="static/brandIcon.png", accountInfo=self.accountInfo)
+            return render_template('profile.html', logo="static/brandIcon.png", accountInfo=user)
         @app.route('/<string:name>', methods=["GET"])
         def _get_javascript_data(name):
             if name == "process":
                 return ", ".join(data[0])
             if(render_template(name)):
                 # Remember: ** We have a profile image, which was added in the accounts section. **
-                return render_template(name, logo="static/brandIcon.png", accountInfo=self.accountInfo)
+                return render_template(name, logo="static/brandIcon.png", accountInfo=user)
             return render_template("404.html")
+        @app.route("/signUp", methods=["POST"])
+        def _():
+            username = request.form.get("accountName")
+            passName = request.form.get("password")
+            successful = user.addClient(username, passName)
+            self.accountInfo = user
+            return "1" if successful else "0"
         @app.route("/<string:name>", methods=["POST"])
-        def getData(name):
+        def _getData(name):
             # Go through our stuff
             username = request.form.get("accountName")
             attemptedPass = request.form.get("password")
-            
-            for i in range(len(accountData["USERNAMES"])):
-                if accountData["USERNAMES"][i] == username:
-                    # Username in database, now check password.
-                    userPassed = 1 if accountData['PASSWORDS'][i] == hashing.hashTag(attemptedPass) else 0
-                    if userPassed:
-                        # The user logged in successfully, now set accountInfo to all our info.
-                        self.accountInfo = {
-                            "name": accountData["USERNAMES"][i],
-                            "info": accountData["INFO"][i],
-                            "names": accountData["NAMES"][i]
-                        }
-                    return str(userPassed)
-            return "0"
+            ans = "1" if user.login(username, attemptedPass) else "0"
+            self.accountInfo = user
+            print(user.theProfileImg)
+            return ans
         if __name__ == '__main__':
             app.run(debug=True)
     
@@ -84,23 +81,23 @@ class Clusters(makeAccount.CSV):
         plt.yticks(np.arange(0, max(self.allCoord[:, 0]) + 10, 1))
         plt.xticks(np.arange(0, max(self.allCoord[:, 1]) + 10, 1))
         # plt.scatter(np.array(x), np.array(y))
-        plt.scatter(self.allCoord[:, 0], self.allCoord[:, 1])
-        for i, txt in enumerate(self.labels):
-            # plt.annotate(txt, (x[i], y[i]))
-            plt.annotate(txt, (self.allCoord[i][0], self.allCoord[i][1]))
-        plt.show()
+        # plt.scatter(self.allCoord[:, 0], self.allCoord[:, 1])
+        # for i, txt in enumerate(self.labels):
+        #     # plt.annotate(txt, (x[i], y[i]))
+        #     plt.annotate(txt, (self.allCoord[i][0], self.allCoord[i][1]))
+        # plt.show()
     def KMeans(self):
         from sklearn.cluster import KMeans as KM
         algorithm = KM(n_clusters=2)
         categories = algorithm.fit_predict(self.allCoord)
-        plt.scatter(self.allCoord[categories == 0, 0], self.allCoord[categories == 0, 1], c= "green")
-        plt.scatter(self.allCoord[categories == 1, 0],self.allCoord[categories == 1, 1], c="red")
-        plt.scatter(algorithm.cluster_centers_[:, 0], algorithm.cluster_centers_[:, 1], c= "black", marker="*")
+        # plt.scatter(self.allCoord[categories == 0, 0], self.allCoord[categories == 0, 1], c= "green")
+        # plt.scatter(self.allCoord[categories == 1, 0],self.allCoord[categories == 1, 1], c="red")
+        # plt.scatter(algorithm.cluster_centers_[:, 0], algorithm.cluster_centers_[:, 1], c= "black", marker="*")
         for i, txt in enumerate(self.labels):
             plt.annotate(txt, (self.allCoord[i][0], self.allCoord[i][1]))
         plt.annotate("NO INFLAMMATION", algorithm.cluster_centers_[0])
         plt.annotate("CAUSES INFLAMMATION", algorithm.cluster_centers_[1])
-        plt.show()
+        # plt.show()
 
 
 i = Clusters()